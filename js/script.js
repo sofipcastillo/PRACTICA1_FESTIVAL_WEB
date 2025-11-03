9ca92263ff88cab0805418f0907ab03b856e8a6e
Sofia Piedrahita <sofipcastillo@gmail.com>
2025-11-03 20:04:26 +0100

    actualizacion con formulario, menu, video y javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// BURGER MENU
 let burger = document.querySelector(".burger > i");
 let menu_opt = document.querySelector(".menu");
 let links = document.querySelectorAll(".menu a");
@@ -10,7 +11,37 @@ burger.addEventListener("click", function () {
 links.forEach((link) => {
   link.addEventListener("click", function () {
     menu_opt.classList.remove("menu-show"); // Oculta el menú
-    burger.classList.add("fa-bars"); // Asegura que el ícono vuelva a "bars"
+    burger.classList.add("fa-bars"); // el ícono vuelva a "bars"
     burger.classList.remove("fa-times");
+  });
+});
+
+//FORMULARIO ENTRADAS
+document.addEventListener("DOMContentLoaded", function () {
+  const inputs = document.querySelectorAll(
+    '#form-entradas input[type="number"]'
+  );
+  const totalPriceElement = document.getElementById("totalPrice");
+  const comprarBtn = document.getElementById("comprarEntradas");
+
+  function calcularTotal() {
+    let total = 0;
+    inputs.forEach((input) => {
+      const precio = parseFloat(input.dataset.price) || 0;
+      const cantidad = parseInt(input.value) || 0;
+      total += precio * cantidad;
+    });
+
+    totalPriceElement.textContent = total.toFixed(2) + " €";
+
+    if (total > 0) {
+      comprarBtn.classList.remove("disabled");
+    } else {
+      comprarBtn.classList.add("disabled");
+    }
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", calcularTotal);
   });
 });
